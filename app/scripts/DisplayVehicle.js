4394c49aaedde7a88220763c43a267e9bdd1c6ac
t.mittlener <t.mittlener@gmail.com>
2017-03-17 14:01:48 +0100

    fixed background

--- app/scripts/DisplayVehicle.js
+++ app/scripts/DisplayVehicle.js
@@ -11,7 +11,7 @@ export const vehicleResponseHandler = (response) => {
 };
 
 const vechDisplay = (vechModels) => {
-  for (let i = 0; i < vechModels.length-7; i++) {
+  for (let i = 0; i < vechModels.length-5; i++) {
     let destination = document.querySelector(".vech-name");
     let node = document.createElement("span");
     let textNode = document.createTextNode(vechModels[i]);
